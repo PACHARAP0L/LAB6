fee3d1339e3d528725562d76bd514f3ecd61c827
PACHARAP0L <pacharapol.sr@ku.th>
2025-08-16 07:15:14 +0700

    Update ProductCatalog.java

--- Lib/ProductCatalog.java
+++ Lib/ProductCatalog.java
@@ -1,14 +1,10 @@
 package Lib;
-
 import java.util.ArrayList;
-
 public class ProductCatalog {
     private ArrayList<Product> products =new ArrayList<>();
-
     //RI: produxts list is not null, contains no null elements,
     // and no duplicates products.
     //AF: AF(products) = A catalog of all availavle products.
-
     private void checkRep() {
         if(products == null){
             throw new RuntimeException("RI violated: products is can not be null");
@@ -17,38 +13,20 @@ public class ProductCatalog {
         for (int i=0; i< products.size(); i++){
             for(int j=i+1; j< products.size(); j++){
                 if (products.get(i).equals(products.get(j))){
-                    throw new RuntimeException("RI violated : Product is can not be duplicate");
-                }
-            }
-        }
-    }
-
-    public ProductCatalog() {
-        checkRep();
-    }
+                    throw new RuntimeException("RI violated : Product is can not be duplicate");}}}}
+    public ProductCatalog() {checkRep();}
     /**
      * เพิ่มสินค้าใหม่เข้าสู่ Catalog
      * @param product สินค้าที่ต้องการเพิ่ม
      */
     public void addProduct(Product product){
         if (product !=null && !products.contains(product)){
-             products.add(product);
-        }
-       checkRep();
-    }
+             products.add(product);}checkRep();}
     /**
      * ค้นหาสินค้าจากรหัสสินค้า
      * @param productId รหัสสินค้าที่ต้องการค้นหา
      * @return อ็อบเจกต์ Product หากพบ, หรือ null หากไม่พบ
      */
-
-
     public Product findById(String productId){
         for (Product p : products){
-            if(p.getProductId().equals(productId)){
-                return p;
-            }
-        }
-        return null;
-    }
-}
+            if(p.getProductId().equals(productId)){return p;}}return null;}}
