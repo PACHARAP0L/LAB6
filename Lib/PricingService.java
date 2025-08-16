81276309022abac9241f92db71a5d8a94d02065e
PACHARAP0L <pacharapol.sr@ku.th>
2025-08-16 07:19:22 +0700

    Update PricingService.java

--- Lib/PricingService.java
+++ Lib/PricingService.java
@@ -3,14 +3,10 @@ package Lib;
 import java.util.ArrayList;
 
 import Lib.Discount.DefaultPricingStrategy;
-
 public class PricingService {
     private record  StrategyRule(String sku, DiscountStrategy strategy) {};
-
     private final ArrayList<StrategyRule> strategies = new ArrayList<>();
-
     private final DiscountStrategy defaultStrategy = new DefaultPricingStrategy();
-
     /**
      * ลงทะเบียนกลยุทธ์ สำหรับส่วนลดสินค้า SKU ที่กําหนด
      * หากมีโปรโมชันสำหรับ SKU  นี้จะถูกแทนที่ด้วยอันใหม่
@@ -22,15 +18,11 @@ public class PricingService {
       for(StrategyRule rule : strategies){
         if (rule.sku().equals(sku)) {
           ruleToRemove = rule;
-          break;
-        }
-      }
+          break;}}
       if (ruleToRemove != null) {
         strategies.remove(ruleToRemove);
-        
-      }
-      strategies.add(new StrategyRule(sku, strategy));
     }
+    strategies.add(new StrategyRule(sku, strategy));}
     /**
      * คำนวณราคาสุทธิของสินค้า 1 รายการโดยใช้กลยุทธ์ที่เหมาะสม
      * @param item รายการสินค้าที่ต้องการคำนวณราคา
